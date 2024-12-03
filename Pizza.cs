87923b8e67bc4538f9da1283bf170d6f602135af
Mouhamed1000 <ndoyenaby12@gmail.com>
2024-12-03 01:45:00 +0000

    Ajout Entity Framework Core à ce projet C#

--- Pizza.cs
+++ Pizza.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+
 namespace PizzaStore.Models
 {
     public class Pizza
@@ -12,5 +14,11 @@ namespace PizzaStore.Models
         public int IdEhod { get; set; }
         public string? NomEhod { get; set; }
         public string? DescriptionEhod { get; set; }
+    }
+
+    class PizzaDb : DbContext
+    {
+        public PizzaDb(DbContextOptions options) : base(options) { }
+        public DbSet<Pizza> Pizzas { get; set; } = null!;
     }
 }
